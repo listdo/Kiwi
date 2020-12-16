e40d56c6e298ea2279ce255c86d075b3e384034d
S1910307072 (List Dominik) <s1910307072@students.fh-hagenberg.at>
2020-12-16 12:45:35 +0100

    Fixed spacing

--- js/script.js
+++ js/script.js
@@ -11,22 +11,27 @@ function handle_reset_input()
     timerState = false;
     timer = 0;
 
+    setDefault();
+}
+
+function setDefault()
+{
     var timerField = jQuery('#timer');
-    timerField.val(new Date(timer * 1000).toISOString().substr(11, 8).toString());
+    timerField.val(new Date(0 * 1000).toISOString().substr(11, 8).toString());
 }
 
 jQuery(document).ready(function() {
+    setDefault();
+    var timerField = jQuery('#timer');
     
-        var timerField = jQuery('#timer');
-      
-        function updateTime() {
-            if(timerState)
-            {
-                timer++;       
-                timerField.val(new Date(timer * 1000).toISOString().substr(11, 8).toString());              
-            }
+    function updateTime() {
+        if(timerState)
+        {
+            timer++;       
+            timerField.val(new Date(timer * 1000).toISOString().substr(11, 8).toString());              
         }
+    }
 
-        updateTime();
-        setInterval(updateTime, 1000);
+    updateTime();
+    setInterval(updateTime, 1000);
 });
