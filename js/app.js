111c695052f6085cfc761f10fa9fa8f878458a1a
S1910307072 (List Dominik) <s1910307072@students.fh-hagenberg.at>
2020-12-21 14:39:32 +0100

    small changes

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ let timerState = false;
 let timer = 0;
 
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('./sw.js')
+    navigator.serviceWorker.register('sw.js')
       .then(reg => console.log('service worker registered'))
       .catch(err => console.log('service worker not registered', err));
 }
