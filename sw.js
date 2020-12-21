49ca2926989457bf78468a4bc8d67b9d90751157
S1910307072 (List Dominik) <s1910307072@students.fh-hagenberg.at>
2020-12-21 13:47:42 +0100

    Added first prototyp for usage as pwa

---
+++ sw.js
@@ -0,0 +1,43 @@
+const staticCacheName = 'kiwi-static-v1';
+const assets = [
+  '/index.html',
+  '/js/app.js',
+  '/css/default.css',
+  '/resources/images/githublogos/GitHub-Mark-32px.png',
+  '/resources/images/Kiwi.png',
+];
+// install event
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
+});
+const dynamicCacheName = 'site-dynamic-v1';
+// activate event
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key =>  key !== dynamicCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+// fetch event
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request).then(fetchRes => {
+        return caches.open(dynamicCacheName).then(cache => {
+          cache.put(evt.request.url, fetchRes.clone());
+          return fetchRes;
+        })
+      });
+    })
+  );
+});
+
